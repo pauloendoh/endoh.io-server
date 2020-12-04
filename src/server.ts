7db7c15963b1cd0e3db02e588c82dc3f82fef8c3
Paulo Endoh <paulo.endoh@gmail.com>
2020-12-04 00:16:49 -0300

    checking process.env

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { createConnection as connectTypeorm } from 'typeorm';
 import { myConsoleError } from './utils/myConsoleError';
 import { myConsoleSuccess } from './utils/myConsoleSuccess';
 
+console.log('process.env: ', process.env)
 connectTypeorm().then(async connection => {
     const app = express()
     app.use(cors())
