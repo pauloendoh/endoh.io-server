25aadf54556c415529c46b84f6f018e9b01f4b74
Paulo Endoh <paulo.endoh@gmail.com>
2021-03-24 14:33:46 -0300

    Returning ormconfig to .js, so I can run the command "typeorm migration:run"

---
+++ ormconfig.js
@@ -0,0 +1,45 @@
+
+require('dotenv').config()
+
+// isso serve apenas para executar as migrations em produção!
+// Roda durante o typeorm migration:run
+const ormconfig = {
+   type: "postgres",
+   host: process.env.NODE_ENV === 'production' ?
+      process.env[DotEnvKeys.DB_HOST] : 'localhost',
+   port: process.env.NODE_ENV === 'production' ?
+      Number(process.env[DotEnvKeys.DB_PORT]) : 5432,
+   username: process.env.NODE_ENV === 'production' ?
+      process.env[DotEnvKeys.DB_USERNAME] : 'postgres',
+   password: process.env.NODE_ENV === 'production' ?
+      process.env[DotEnvKeys.DB_PASSWORD] : 'password',
+   database: "endoh.io",
+   entities: [
+      __dirname + "/src/entities/**/*.ts",
+      __dirname + "/src/entities/**/*.js",
+   ],
+   synchronize: false,
+   logging:
+      true,
+
+   migrations: [
+      __dirname + "/src/migrations/**/*.ts",
+      __dirname + "/src/migrations/**/*.js",
+   ],
+   subscribers: [
+      __dirname + "/src/subscriber/**/*.ts",
+      __dirname + "/src/subscriber/**/*.js",
+   ],
+   cli: {
+      "entitiesDir":
+         process.env.NODE_ENV === 'production' ? "build/src/entities" : "src/entities",
+      "migrationsDir":
+         process.env.NODE_ENV === 'production' ? "build/src/migrations" : "src/migrations",
+      "subscribersDir":
+         process.env.NODE_ENV === 'production' ? "build/src/subscriber" : "src/subscriber",
+
+   },
+}
+
+
+module.exports = ormconfig
