1a1f23fdb0a6f96c8de0a9a424d9e62986a86fea
Paulo Endoh <paulo.endoh@gmail.com>
2021-07-13 07:40:41 -0300

    trying github actions to npm run build and heroku deploy

--- build/config/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.YOUTUBE_API_KEY = void 0;
-require(`dotenv`).config();
-exports.YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
--- build/utils/email/isValidEmail.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidEmail = void 0;
-const isValidEmail = (text) => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(text).toLowerCase());
-};
-exports.isValidEmail = isValidEmail;
