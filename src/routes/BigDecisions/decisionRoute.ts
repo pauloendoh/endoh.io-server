f538b3b8d9d7c706552ac090e205c26449097d16
Paulo Endoh <paulo.endoh@gmail.com>
2022-02-13 09:26:51 -0300

    Updating big decisions routes to v2

--- src/routes/BigDecisions/decisionRoute.ts
+++ src/routes/BigDecisions/decisionRoute.ts
@@ -1,85 +1,16 @@
-import { Router } from "express"
-import { getCustomRepository } from "typeorm"
-import { Decision } from "../../entities/BigDecisions/Decision"
-import authMiddleware from "../../middlewares/authMiddleware"
-import DecisionRepository from "../../repositories/BigDecisions/DecisionRepository"
-import { MyErrorsResponse } from "../../utils/ErrorMessage"
-import { MyAuthRequest } from "../../utils/MyAuthRequest"
-import { myConsoleError } from "../../utils/myConsoleError"
+import { Router } from "express";
+import { getCustomRepository } from "typeorm";
+import authMiddleware from "../../middlewares/authMiddleware";
+import DecisionRepository from "../../repositories/BigDecisions/DecisionRepository";
+import { MyAuthRequest } from "../../utils/MyAuthRequest";
 
-const decisionRoute = Router()
-const decisionRepo = getCustomRepository(DecisionRepository)
-
-decisionRoute.get("/", authMiddleware, async (req: MyAuthRequest, res) => {
-  try {
-    const allDecisions = await decisionRepo.getAllFromUser(req.user.id)
-    return res.status(200).json(allDecisions)
-  } catch (err) {
-    myConsoleError(err.message)
-    return res.status(400).json(new MyErrorsResponse(err.message))
-  }
-})
-
-decisionRoute.post("/", authMiddleware, async (req: MyAuthRequest, res) => {
-  const sentDecision = req.body as Decision
-  const { user } = req
-
-  try {
-    if (sentDecision.id) {
-      // TODO
-      // check ownership
-      const decision = await decisionRepo.findOne({
-        where: { id: sentDecision.id, userId: user.id },
-      })
-
-      if (!decision) {
-        return res
-          .status(400)
-          .json(
-            new MyErrorsResponse(`Doc doesn't exist or user doesn't own it.`)
-          )
-      }
-    }
-
-    sentDecision.userId = req.user.id
-    const saved = await decisionRepo.save(sentDecision)
-    const fullSaved = await decisionRepo.getFullDecision(saved.id)
-
-    // const allDecisions = await decisionRepo.getAllFromUser(req.user.id)
-    return res.status(200).json(fullSaved)
-  } catch (err) {
-    myConsoleError(err.message)
-    return res.status(400).json(new MyErrorsResponse(err.message))
-  }
-})
+const decisionRoute = Router();
+const decisionRepo = getCustomRepository(DecisionRepository);
 
 decisionRoute.delete(
   "/:id",
   authMiddleware,
-  async (req: MyAuthRequest, res) => {
-    const decisionId = parseFloat(req.params.id)
+  async (req: MyAuthRequest, res) => {}
+);
 
-    try {
-      const decision = await decisionRepo.findOne({
-        where: { id: decisionId, userId: req.user.id },
-      })
-
-      if (!decision) {
-        return res
-          .status(400)
-          .json(
-            new MyErrorsResponse(`Doc doesn't exist or user doesn't own it.`)
-          )
-      }
-
-      await decisionRepo.remove(decision)
-      return res.status(200).json()
-
-    } catch (err) {
-      myConsoleError(err.message)
-      return res.status(400).json(new MyErrorsResponse(err.message))
-    }
-  }
-)
-
-export default decisionRoute
+export default decisionRoute;
