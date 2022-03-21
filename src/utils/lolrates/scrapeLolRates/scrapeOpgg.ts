a8c65fd77ba15024be2b1fd17df54ddb167151df
Paulo Endoh <paulo.endoh@gmail.com>
2022-03-21 15:41:13 -0300

    Updating op.gg scraper

--- src/utils/lolrates/scrapeLolRates/scrapeOpgg.ts
+++ src/utils/lolrates/scrapeLolRates/scrapeOpgg.ts
@@ -1,72 +1,78 @@
-import * as pup from "puppeteer"
-import { getCustomRepository } from "typeorm"
-import LolRateRepository from "../../../repositories/lolrates/LolRateRepository"
-import { myConsoleError } from "../../myConsoleError"
-import { RoleTypes } from "../../../types/domain/lolates/RoleTypes"
+import { Page } from "puppeteer";
+import { getCustomRepository } from "typeorm";
+import LolRateRepository from "../../../repositories/lolrates/LolRateRepository";
+import { RoleTypes } from "../../../types/domain/lolates/RoleTypes";
+import { myConsoleError } from "../../myConsoleError";
+import { myConsoleSuccess } from "../../myConsoleSuccess";
+import { sleep } from "../../sleep";
 
 export interface IOpggResult {
-  role: string
-  championName: string
-  winRate: string
-  pickRate: string
+  role: string;
+  championName: string;
+  winRate: string;
+  pickRate: string;
 }
 
-export async function scrapeOpgg(page: pup.Page) {
+export async function scrapeOpgg(page: Page) {
+  myConsoleSuccess("Starting scrapeOpgg");
   try {
-    await page.goto("https://www.op.gg/champion/statistics")
+    const positions: string[] = ["top", "jungle", "mid", "adc", "support"];
 
-    await page.waitForSelector(
-      '[data-tab-show-class="champion-trend-winratio"]'
-    )
+    for (const position of positions) {
+      await page.goto(`https://www.op.gg/champions?position=${position}`);
 
-    const results = await page.evaluate(() => {
-      function getRoleByTBodyIndex(index: number): RoleTypes {
-        switch (index) {
-          case 0:
-            return "TOP"
-          case 1:
-            return "JUNGLE"
-          case 2:
-            return "MID"
-          case 3:
-            return "BOT"
-          case 4:
-            return "SUP"
+      await page.waitForSelector("tbody");
+      await sleep(500); // waiting for champions sort
+
+      const results = await page.evaluate(() => {
+        function getRole(): RoleTypes {
+          const urlSearchParams = new URLSearchParams(window.location.search);
+          const params = Object.fromEntries(urlSearchParams.entries());
+
+          switch (params.position) {
+            case "top":
+              return "TOP";
+            case "jungle":
+              return "JUNGLE";
+            case "mid":
+              return "MID";
+            case "adc":
+              return "BOT";
+            case "support":
+              return "SUP";
+          }
         }
-      }
 
-      const results: IOpggResult[] = []
-
-      const tbodies = Array.from(document.querySelectorAll("tbody"))
-      let tbodyIndex = 0
-      for (const tbody of tbodies) {
-        const trs = Array.from(tbody.querySelectorAll("tr"))
+        const results: IOpggResult[] = [];
+        const tbody = document.querySelector("tbody");
+        const trs = Array.from(tbody.querySelectorAll("tr"));
 
         for (const tr of trs) {
-          const tds = tr.querySelectorAll("td")
-          const championTd = tds[3]
-          const championName = championTd.querySelector(
-            ".champion-index-table__name"
-          ).textContent
-          const winRate = tds[4].innerText
-          const pickRate = tds[5].innerText
+          const tds = tr.querySelectorAll("td");
+          const championTd = tds[1];
+          const championName = championTd.querySelector("strong").textContent;
+          const winRate = tds[2].innerText;
+          const pickRate = tds[3].innerText;
 
           results.push({
-            role: getRoleByTBodyIndex(tbodyIndex),
+            role: getRole(),
             championName,
             winRate,
             pickRate,
-          })
+          });
         }
-        tbodyIndex++
-      }
-      return results
-    })
 
-    const saved = await getCustomRepository(LolRateRepository).saveOpgg(results)
+        return results;
+      });
 
-    return
+      const saved = await getCustomRepository(LolRateRepository).saveOpgg(
+        results
+      );
+      myConsoleSuccess(`OP GG - ${position} OK}`);
+    }
+
+    return;
   } catch (err) {
-    myConsoleError(err.message)
+    myConsoleError(err.message);
   }
 }
