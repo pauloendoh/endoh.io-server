4a8f38090d5a2bc0057bd19b7959b4e255a770a1
Paulo Endoh <paulo.endoh@gmail.com>
2022-06-01 11:04:38 -0300

    Fix email password reset with sendgrid

--- src/utils/email/sendPasswordResetEmail.ts
+++ src/utils/email/sendPasswordResetEmail.ts
@@ -1,61 +1,56 @@
-"use strict";
-
-import { randomBytes } from 'crypto';
-import { getRepository } from 'typeorm';
-import { USER_TOKEN_TYPES } from '../../consts/USER_TOKEN_TYPES';
-import { UserToken } from '../../entities/OAuthToken';
-import { User } from '../../entities/User';
-import { myConsoleSuccess } from '../myConsoleSuccess';
-import { addMinutes } from '../time/addMinutes';
+import * as sgMail from "@sendgrid/mail";
+import { randomBytes } from "crypto";
+import { getRepository } from "typeorm";
+import { USER_TOKEN_TYPES } from "../../consts/USER_TOKEN_TYPES";
+import { UserToken } from "../../entities/OAuthToken";
+import { User } from "../../entities/User";
+import { myConsoleSuccess } from "../myConsoleSuccess";
+import { addMinutes } from "../time/addMinutes";
 
 const nodemailer = require("nodemailer");
-require('dotenv').config()
+require("dotenv").config();
 
 // async..await is not allowed in global scope, must use a wrapper
 export async function sendPasswordResetEmail(user: User) {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    // let testAccount = await nodemailer.createTestAccount();
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  // let testAccount = await nodemailer.createTestAccount();
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'endohpa@gmail.com', // generated ethereal user
-            pass: process.env.EMAIL_PASS, // generated ethereal password
-        },
-    });
+  const userTokenRepo = getRepository(UserToken);
 
-    const userTokenRepo = getRepository(UserToken)
+  // removes all the previous password reset tokens from the user
+  await userTokenRepo.delete({
+    userId: user.id,
+    type: USER_TOKEN_TYPES.passwordReset,
+  });
 
-    // removes all the previous password reset tokens from the user 
-    await userTokenRepo.delete({
-        userId: user.id,
-        type: USER_TOKEN_TYPES.passwordReset
-    })
+  const token = await userTokenRepo.save({
+    userId: user.id,
+    type: USER_TOKEN_TYPES.passwordReset,
+    token: randomBytes(64).toString("hex"),
+    expiresAt: addMinutes(new Date(), 15).toISOString(),
+  });
 
-    const token = await userTokenRepo.save({
-        userId: user.id,
-        type: USER_TOKEN_TYPES.passwordReset,
-        token: randomBytes(64).toString('hex'),
-        expiresAt: addMinutes(new Date(), 15).toISOString()
-    })
+  const url =
+    process.env.CLIENT_BASE_URL +
+    "/password-reset?token=" +
+    token.token +
+    "&userId=" +
+    user.id;
 
-    const url = process.env.CLIENT_BASE_URL + "/password-reset?token=" + token.token + "&userId=" + user.id
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-    // send mail with defined transport object
-    let sentEmailInfo = await transporter.sendMail({
-        from: '"Endoh.io" <endohpa@gmail.com>', // sender address
-        to: user.email, // list of receivers
-        subject: "Endoh.io - Password reset", // Subject line
-        text: "Enter this link to complete your password reset: ",
-        html: `Enter this link to complete your password reset: <br/>
+  const responses = await sgMail.send({
+    from: "endohpa@gmail.com",
+    to: user.email, // list of receivers
+    subject: "Endoh.io - Password reset", // Subject line
+    text: "Enter this link to complete your password reset: ",
+    html: `Enter this link to complete your password reset: <br/>
             <a href="${url}">${url}</a>
         `,
-    });
+  });
 
-    myConsoleSuccess("Message sent: " + sentEmailInfo.messageId)
+  myConsoleSuccess(responses[0].toString());
 
+  return responses;
 }
-
-
