bf82f63a2d5af70975b733c403b9a8cc08719f6a
Paulo Endoh <paulo.endoh@gmail.com>
2022-06-29 13:44:00 -0300

    Updating first migration: adding extension 'unaccent'

--- src/migrations/1607450946893-Migration.ts
+++ src/migrations/1607450946893-Migration.ts
@@ -4,6 +4,8 @@ export class Migration1607450946893 implements MigrationInterface {
   name = "Migration1607450946893";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "unaccent";`);
+
     await queryRunner.query(
       `CREATE TABLE "user" ("id" SERIAL NOT NULL, "username" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "isAdmin" boolean NOT NULL DEFAULT false, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`
     );
@@ -99,5 +101,7 @@ export class Migration1607450946893 implements MigrationInterface {
     );
 
     await queryRunner.query(`DROP TABLE "user"`);
+
+    await queryRunner.query(`DROP EXTENSION "unaccent";`);
   }
 }
