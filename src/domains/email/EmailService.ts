b825c90ede296d54b9277770b3e44c081de81403
Paulo Endoh <paulo.endoh@gmail.com>
2022-10-15 09:10:58 -0300

    feat/cl99u8n9h28790mx2by5z41kq - new user email

---
+++ src/domains/email/EmailService.ts
@@ -0,0 +1,34 @@
+import { MailService } from "@sendgrid/mail"
+import { User } from "../../entities/User"
+
+export class EmailService {
+  constructor(private sendgridService = new MailService()) {
+    this.sendgridService.setApiKey(process.env.SENDGRID_API_KEY)
+  }
+
+  notifyNewUserToDevs(user: User) {
+    const devsEmailsString = process.env.NOTIFY_NEW_USER_TO_DEVS
+
+    if (!devsEmailsString?.trim()) return
+
+    const devsEmails = devsEmailsString.split(";").map((s) => s.trim())
+
+    const subject = user.expiresAt
+      ? "[endoh.io] - New TEMP user"
+      : "[endoh.io] - New user: " + user.username
+
+    this.sendgridService
+      .send({
+        to: devsEmails,
+        from: "endohpa@gmail.com",
+        subject,
+        text: user.email,
+      })
+      .then(() => {
+        console.log("Email sent!")
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
+  }
+}
--- src/subscriber/UserSubscriber.ts
+++ src/subscriber/UserSubscriber.ts
@@ -2,73 +2,77 @@ import {
   EntitySubscriberInterface,
   EventSubscriber,
   InsertEvent,
-} from "typeorm";
-import { Profile } from "../entities/feed/Profile";
-import { User } from "../entities/User";
-import { UserPreference } from "../entities/UserPreference";
-import DocRepository from "../repositories/define/DocRepository";
-import NoteRepository from "../repositories/define/NoteRepository";
-import UserSuggestionRepository from "../repositories/feed/UserSuggestionRepository";
-import ResourceRepository from "../repositories/relearn/ResourceRepository";
-import TagRepository from "../repositories/relearn/TagRepository";
-import SkillExpectationRepository from "../repositories/skillbase/SkillExpectationRepository";
-import SkillRepository from "../repositories/skillbase/SkillRepository";
-import { myConsoleError } from "../utils/myConsoleError";
+} from "typeorm"
+import { EmailService } from "../domains/email/EmailService"
+import { Profile } from "../entities/feed/Profile"
+import { User } from "../entities/User"
+import { UserPreference } from "../entities/UserPreference"
+import DocRepository from "../repositories/define/DocRepository"
+import NoteRepository from "../repositories/define/NoteRepository"
+import UserSuggestionRepository from "../repositories/feed/UserSuggestionRepository"
+import ResourceRepository from "../repositories/relearn/ResourceRepository"
+import TagRepository from "../repositories/relearn/TagRepository"
+import SkillExpectationRepository from "../repositories/skillbase/SkillExpectationRepository"
+import SkillRepository from "../repositories/skillbase/SkillRepository"
+import { myConsoleError } from "../utils/myConsoleError"
 
 @EventSubscriber()
 export class UserSubscriber implements EntitySubscriberInterface<User> {
+  constructor(private emailService = new EmailService()) {}
   listenTo() {
-    return User;
+    return User
   }
 
   async afterInsert(event: InsertEvent<User>) {
     try {
-      const preference = new UserPreference();
-      preference.user = event.entity;
+      const preference = new UserPreference()
+      preference.user = event.entity
+
+      this.emailService.notifyNewUserToDevs(event.entity)
 
       // YOU HAVE TO USE event.manger.getRepository in order to make timely changes
-      await event.manager.getRepository(UserPreference).save(preference);
+      await event.manager.getRepository(UserPreference).save(preference)
 
-      const profile = new Profile();
-      profile.user = event.entity;
-      await event.manager.getRepository(Profile).save(profile);
+      const profile = new Profile()
+      profile.user = event.entity
+      await event.manager.getRepository(Profile).save(profile)
 
       // Create user suggestions
       await event.manager
         .getCustomRepository(UserSuggestionRepository)
-        .createUserSuggestionsForUser(event.entity);
+        .createUserSuggestionsForUser(event.entity)
 
       // Create user tags
       const tags = await event.manager
         .getCustomRepository(TagRepository)
-        .createExampleTagsForNewUser(event.entity);
+        .createExampleTagsForNewUser(event.entity)
 
       // Create resources
       await event.manager
         .getCustomRepository(ResourceRepository)
-        .createResourcesForNewUser(event.entity, tags);
+        .createResourcesForNewUser(event.entity, tags)
 
       // Create skill to Programming tag
       const skills = await event.manager
         .getCustomRepository(SkillRepository)
-        .createSkillsForNewUser(event.entity, tags[0]);
+        .createSkillsForNewUser(event.entity, tags[0])
 
       // Create skill expectations
       await event.manager
         .getCustomRepository(SkillExpectationRepository)
-        .createExpectationsForNewUser(event.entity, skills);
+        .createExpectationsForNewUser(event.entity, skills)
 
       // Create little prince doc
       const doc = await event.manager
         .getCustomRepository(DocRepository)
-        .createDocForNewUser(event.entity);
+        .createDocForNewUser(event.entity)
 
       // Create little prince notes
       await event.manager
         .getCustomRepository(NoteRepository)
-        .createNotesForNewUser(event.entity, doc);
+        .createNotesForNewUser(event.entity, doc)
     } catch (e) {
-      myConsoleError(e.message);
+      myConsoleError(e.message)
     }
   }
 }
