e8d9a3427e7e945cb7449f7ceb326cbfac341c43
Paulo Endoh <paulo.endoh@gmail.com>
2022-10-20 20:32:46 -0300

    fixing build

--- src/migrations/1666308697756-Migration.ts
+++ src/migrations/1666308697756-Migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migration1666306475737 implements MigrationInterface {
-    name = 'Migration1666306475737'
+export class Migration1666308697756 implements MigrationInterface {
+    name = 'Migration1666308697756'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "test" ADD "isTrue" boolean NOT NULL DEFAULT false`);
