5036ec2085cdf8557b23d4d4ced7ee82f60d437f
Paulo Endoh <paulo.endoh@gmail.com>
2025-03-15 12:48:30 -0300

    reducing logs

--- src/utils/runMyJSDOM.ts
+++ src/utils/runMyJSDOM.ts
@@ -5,6 +5,10 @@ export function myJSDOMDocument(html: string) {
   try {
     const dom = new JSDOM(html)
 
+    dom.virtualConsole.on("error", () => {
+      myConsoleError("JSDOM error")
+    })
+
     return dom.window.document
   } catch (err) {
     myConsoleError("Error parsing html. Stack: " + err.stack)
