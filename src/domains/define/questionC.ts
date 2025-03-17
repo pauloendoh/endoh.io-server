b474657035ac8416c6a274aa047aff1ab2be5473
Paulo Endoh <paulo.endoh@gmail.com>
2025-03-16 21:50:24 -0300

    refactor

--- src/domains/define/questionC.ts
+++ src/domains/define/questionC.ts
@@ -6,33 +6,7 @@ import { questionSchema } from "../../entities/define/Question"
 
 const c = initContract()
 
-const postSchema = z.object({
-  id: z.string(),
-  title: z.string(),
-  body: z.string(),
-})
-
 export const questionC = c.router({
-  createPost: {
-    method: "POST",
-    path: "/posts",
-    responses: {
-      201: postSchema,
-    },
-    body: z.object({
-      title: z.string(),
-      body: z.string(),
-    }),
-    summary: "Create a post",
-  },
-  getPost: {
-    method: "GET",
-    path: `/posts/:id`,
-    responses: {
-      200: postSchema.nullable(),
-    },
-    summary: "Get a post by id",
-  },
   getAllMyQuestions: {
     method: "GET",
     path: "/define/question",
--- src/domains/define/questionRouter.ts
+++ src/domains/define/questionRouter.ts
@@ -1,33 +1,13 @@
 import { RouterImplementation } from "@ts-rest/express/src/lib/types"
-import { authMiddleware } from "../../middlewares/authMiddleware"
+import { userRequiredMiddleware } from "../../middlewares/userRequiredMiddleware"
 import { getQuestionRepository } from "../../repositories/define/QuestionRepository"
 import { contract } from "../contract"
-
-const posts: { id: string; title: string; body: string }[] = []
 
 export const questionRouter: RouterImplementation<
   (typeof contract)["question"]
 > = {
-  getPost: async ({ params: { id } }) => {
-    const post = posts.find((post) => post.id === id)
-
-    return {
-      status: 200,
-      body: post ?? null,
-    }
-  },
-  createPost: async ({ body }) => {
-    const post = { ...body, id: Math.random().toString() }
-
-    posts.push(post)
-
-    return {
-      status: 201,
-      body: post,
-    }
-  },
   getAllMyQuestions: {
-    middleware: [authMiddleware],
+    middleware: [userRequiredMiddleware],
     handler: async ({ req }) => {
       const questionRepo = getQuestionRepository()
       const questions = await questionRepo.getAllQuestionsFromUserId(
